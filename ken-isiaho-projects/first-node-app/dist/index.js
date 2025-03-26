3c0b2d9d8ef48445484b757b5102331c698d6483
Ken Isiaho <kenisiaho@gmail.com>
2025-03-26 17:08:17 +0300

    Delete ken-isiaho-projects directory

--- ken-isiaho-projects/first-node-app/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sum(a, b) {
-    return a + b;
-}
-const result = sum(4, 5);
-console.log(result); 
--- ken-isiaho-projects/first-node-app/index.ts
+++
@@ -1,3 +0,0 @@
-(function sum (a:number, b:number) {
-  console.log(a + b);
-})(4, 5); // 9
